ffae8aafec6dc5f31f48724c82c1d066e941f9f2
naty55 <64107292+naty55@users.noreply.github.com>
2024-11-24 01:58:12 +0200

    fix config stuff

--- client/config.hpp
+++ client/config.hpp
@@ -1,12 +1,15 @@
 #ifndef CLIENT_CONFIG_HPP
 #define CLIENT_CONFIG_HPP
+#include <string>
+
 
 // General
 const int BUFFER_SIZE = 1024;
+const std::string CLIENT_MESSAGE = "MeesageU Client at your service.\n\n110) Register \n120) Request for clients list \n130) Request for public key \n140) Request for waiting messages\n150) Send a text message \n151) Send a request for symmetric key \n152) Send your symmetric key \n0) Exit client";
 
 // Server 
-const int SERVER_PORT = 1357;
-// char SERVER_HOST[] = "localhost";
+const char SERVER_PORT[] = "1357";
+const char SERVER_HOST[] = "localhost";
 
 
 // Protocol
