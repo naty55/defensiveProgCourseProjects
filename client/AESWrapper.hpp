22ffc6ec7b69ca24deab5ec4b6324cb032843f6c
naty55 <64107292+naty55@users.noreply.github.com>
2025-02-23 21:25:34 +0200

    Crypto wrappers from course lecturers

---
+++ client/AESWrapper.hpp
@@ -0,0 +1,32 @@
+#pragma once
+/**
+ * @file AESWrapper.hpp
+ * @author Course Lecturers
+ * @brief 
+ * @version 0.1
+ * @date 2025-02-23
+ * 
+ */
+
+#include <string>
+
+
+class AESWrapper
+{
+public:
+	static const unsigned int DEFAULT_KEYLENGTH = 16;
+private:
+	unsigned char _key[DEFAULT_KEYLENGTH];
+	AESWrapper(const AESWrapper& aes);
+public:
+	static unsigned char* GenerateKey(unsigned char* buffer, unsigned int length);
+
+	AESWrapper();
+	AESWrapper(const unsigned char* key, unsigned int size);
+	~AESWrapper();
+
+	const unsigned char* getKey() const;
+
+	std::string encrypt(const char* plain, unsigned int length);
+	std::string decrypt(const char* cipher, unsigned int length);
+};
