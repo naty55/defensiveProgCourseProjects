64d38477acb06e78b21cae0a632e7e9aad087300
naty55 <64107292+naty55@users.noreply.github.com>
2025-03-21 16:42:45 +0200

    server small refactoring

--- server/protocol/objects.py
+++ server/protocol/objects.py
@@ -1,14 +1,18 @@
 import struct 
 from logs import logger
-class Request:
-    header_format = "<16sBHI"
+import random
+from dataclasses import dataclass
 
-    def __init__(self, client_id, version, code, payload_size, payload):
-        self.cid = client_id
-        self.client_version = version
-        self.req_code = code
-        self.payload_size = payload_size
-        self.payload = payload
+@dataclass
+class Request:
+    cid: bytes
+    client_version: int
+    req_code: int
+    payload_size: int
+    payload: bytes
+
+    #### STATIC VARIABLES ####
+    header_format = "<16sBHI" 
     
     def __str__(self):
         return f"[Request] client_id={self.cid}, client_version={self.client_version}, code={self.req_code}, payload_size={self.payload_size}, payload={self.payload}"
@@ -33,14 +37,15 @@ class Request:
         return struct.calcsize(Request.header_format)
     
 
-
+@dataclass
 class Response:
+    server_version: int
+    code: int
+    payload_size: int
+    payload: bytes
+
+    #### STATIC VARIABLES ####
     header_format = "<BHI"
-    def __init__(self, server_version, response_code, payload_size, payload):
-        self.server_version = server_version
-        self.code = response_code
-        self.payload_size = payload_size
-        self.payload = payload
     
     def to_bytes(self):
         return struct.pack(self.header_format, self.server_version, self.code, self.payload_size) + (self.payload if self.payload != None else b'')
@@ -73,4 +78,4 @@ class Message:
     
     @staticmethod
     def generate_msg_id():
-        return b'\x11\x12\x13\x14'
+        return random.randbytes(4)
